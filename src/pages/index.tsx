476a87317376fbbdfdfcf2ec34b6939690cf1785
DevMboo <lchavesdesousa@gmail.com>
2025-05-09 09:33:57 -0300

    feat: page index formated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { withAuth } from "@/utils/withAuth";
+import Link from "next/link";
 
 function HomePage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="text-center">
+      <div className="text-center space-y-2">
         <h1>Seja bem vindo, você está no Task Plus</h1>
         <p>Meu projeto desafio com React JS e Sprint Boot.</p>
+        <Link href={"/tasks"} className="py-3 px-3 rounded-md bg-purple-600 text-white mt-10">Ir para página inicial</Link>
       </div>
     </div>
   );
